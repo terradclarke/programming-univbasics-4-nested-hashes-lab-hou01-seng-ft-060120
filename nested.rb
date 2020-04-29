6aadf175b5b4940075035d11c619856d21169114
Terranie Sims <terranie67@gmail.com>
2020-04-29 00:40:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,13 +81,13 @@ def adding_matz
       }
     }
   
- 
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]
+  
+  return programmer_hash
 
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -105,15 +105,13 @@ def changing_alan
       }
     }
     
-  programmer_hash[:alan_kay][:known_for] = "GUI
+  programmer_hash[:alan_kay][:known_for] = "GUI"
   
   return programmer_hash
 
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -130,5 +128,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  return programmer_hash
 
 end
